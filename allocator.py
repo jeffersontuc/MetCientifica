0fd3e07aa77455c2a89a2913c297476af99fad73
Jefferson Carlos Benedito <jefiim.ad@gmail.com>
2017-04-02 01:06:04 -0300

    project done

---
+++ allocator.py
@@ -0,0 +1,160 @@
+#coding: utf-8
+
+orders = [
+['best-fit', 0.20, 0.02], 
+['best-fit', 0.55, 0.02], 
+['best-fit', 1, 0.02], 
+['best-fit', 0.20, 0.1], 
+['best-fit', 0.55, 0.1], 
+['best-fit', 1, 0.1], 
+['best-fit', 0.20, 0.4], 
+['best-fit', 0.55, 0.4], 
+['best-fit', 1, 0.4],
+['worst-fit', 0.20, 0.02], 
+['worst-fit', 0.55, 0.02], 
+['worst-fit', 1, 0.02], 
+['worst-fit', 0.20, 0.1], 
+['worst-fit', 0.55, 0.1], 
+['worst-fit', 1, 0.1], 
+['worst-fit', 0.20, 0.4], 
+['worst-fit', 0.55, 0.4], 
+['worst-fit', 1, 0.4] ]
+
+def worstFit(blocs, process):
+	
+	qtdAlocadas = 0
+	
+	for i in range(len(process)):
+
+		crrIdx = -1
+		for j in range(len(blocs)):
+			if(blocs[j][0] >= process[i][0] and blocs[j][1] >= process[i][1]):
+				if(crrIdx == -1):
+					crrIdx = j
+				elif(blocs[crrIdx][0] > blocs[j][0]):
+					crrIdx = j
+
+		if(crrIdx != -1):
+			qtdAlocadas += 1
+			blocs[crrIdx][0] -= process[i][0]
+			blocs[crrIdx][1] -= process[i][1]
+
+	return (qtdAlocadas, blocs)
+
+
+def bestFit(blocs, process):
+	
+	qtdAlocadas = 0
+	
+	for i in range(len(process)):
+
+		crrIdx = -1
+		for j in range(len(blocs)):
+			if(blocs[j][0] >= process[i][0] and blocs[j][1] >= process[i][1]):
+				if(crrIdx == -1):
+					crrIdx = j
+				elif(blocs[crrIdx][0] > blocs[j][0]):
+					crrIdx = j
+
+		if(crrIdx != -1):
+			qtdAlocadas += 1
+			blocs[crrIdx][0] -= process[i][0]
+			blocs[crrIdx][1] -= process[i][1]
+
+	return (qtdAlocadas, blocs)
+
+
+def main():
+	toFile = ""
+	for order in range(len(orders)):
+		process = []
+		blocs = []
+		for i in range(10):
+			blocs.append([orders[order][1], orders[order][1]])
+		if(orders[order][0] == 'best-fit'):
+			if(orders[order][2] == 0.02):
+				file = open("task_02.csv")
+				linhas = file.readlines()
+			elif(orders[order][2] == 0.1):
+				file = open("task_01.csv")
+				linhas = file.readlines()
+			else:
+				file = open("task_04.csv")
+				linhas = file.readlines()
+			for i in range(len(linhas)):
+			    linha = linhas[i].strip().split(';')
+			    cpu = float(linha[8])
+			    memoria = float(linha[9])
+			    process.append([cpu, memoria])
+			 
+			result = bestFit(blocs, process)
+
+			fragmentacaoMemoria = 0
+			fragmentacaoCpu = 0
+
+			for fragMen in range(len(result[1])):
+				fragmentacaoMemoria += result[1][fragMen][1]
+
+			for fragCpu in range(len(result[1])):
+				fragmentacaoCpu += result[1][fragCpu][0]
+
+			toFile += "Recurso requisitado por VM: 0.02" + "\n"
+			toFile += "Quantidade de máquinas físicas: 10" + "\n"
+			toFile += "Capacidade de CPU: " + str(orders[order][1]) + "\n"
+			toFile += "Capacidade de Memória: " + str(orders[order][1]) + "\n"
+			toFile += "Algoritmo executado: Best Fit" + "\n"
+			toFile += "Fragmentação de CPU: " + str(fragmentacaoCpu) + "\n"
+			toFile += "Fragmentação de memória: " + str(fragmentacaoMemoria) + "\n"
+			toFile += "Quantidade de requisições " + str(len(process)) + "\n"
+			toFile += "Quantidade de VMs alocadas: " + str(result[0]) + "\n"
+			toFile += "Quantidade de VMs rejeitadas: " + str(len(process) - result[0]) + "\n"
+			toFile += "=================================================================================" + "\n"
+			toFile += "\n"
+
+		elif(orders[order][0] == 'worst-fit'):
+			if(orders[order][2] == 0.02):
+				file = open("task_02.csv")
+				linhas = file.readlines()
+			elif(orders[order][2] == 0.1):
+				file = open("task_01.csv")
+				linhas = file.readlines()
+			else:
+				file = open("task_04.csv")
+				linhas = file.readlines()
+			for i in range(len(linhas)):
+			    linha = linhas[i].strip().split(';')
+			    cpu = float(linha[8])
+			    memoria = float(linha[9])
+			    process.append([cpu, memoria])
+			 
+			result = bestFit(blocs, process)
+
+			fragmentacaoMemoria = 0
+			fragmentacaoCpu = 0
+
+			for fragMen in range(len(result[1])):
+				fragmentacaoMemoria += result[1][fragMen][1]
+
+			for fragCpu in range(len(result[1])):
+				fragmentacaoCpu += result[1][fragCpu][0]
+
+			toFile += "Recurso requisitado por VM: 0.02" + "\n"
+			toFile += "Quantidade de máquinas físicas: 10" + "\n"
+			toFile += "Capacidade de CPU: " + str(orders[order][1]) + "\n"
+			toFile += "Capacidade de Memória: " + str(orders[order][1]) + "\n"
+			toFile += "Algoritmo executado: Worst Fit" + "\n"
+			toFile += "Fragmentação de CPU: " + str(fragmentacaoCpu) + "\n"
+			toFile += "Fragmentação de memória: " + str(fragmentacaoMemoria) + "\n"
+			toFile += "Quantidade de requisições " + str(len(process)) + "\n"
+			toFile += "Quantidade de VMs alocadas: " + str(result[0]) + "\n"
+			toFile += "Quantidade de VMs rejeitadas: " + str(len(process) - result[0]) + "\n"
+			toFile += "=================================================================================" + "\n"
+			toFile += "\n"
+
+	arquivo = open("log_result.txt", "w")
+	arquivo.writelines(toFile.strip())
+	arquivo.close()
+
+
+main()
+
---
+++ separator.py
@@ -0,0 +1,61 @@
+# coding: utf-8
+
+import random
+import os
+
+def remove(path):
+    if os.path.isfile(path):
+        os.remove(path)  # remove the file
+    elif os.path.isdir(path):
+        shutil.rmtree(path)  # remove dir and all contains
+    else:
+        raise ValueError("file {} is not a file or dir.", format(path))
+
+def separar_dados():
+    file = open("task_events.csv")
+    linhas = file.readlines()
+
+    tasks_02 = []
+    tasks_1 = []
+    tasks_4 = []
+    
+    remove("log_result.txt")
+
+
+    for i in range(1, len(linhas)):
+        linha = linhas[i].strip().split(";")
+        if float(linha[8]) <= 0.02:
+            tasks_02.append(linhas[i].strip())
+        elif float(linha[8]) <= 0.1:
+            tasks_1.append(linhas[i].strip())
+        else:
+            tasks_4.append(linhas[i].strip())
+
+    random.shuffle(tasks_02)
+    random.shuffle(tasks_1)
+    random.shuffle(tasks_4)
+
+    task_02 = ""
+    task_1 = ""
+    task_4 = ""
+
+    menor = min([len(tasks_02), len(tasks_1), len(tasks_4)])
+
+    for i in range(menor):
+        task_02 += tasks_02[i] + "\n"
+        task_1 += tasks_1[i] + "\n"
+        task_4 += tasks_4[i] + "\n"
+
+    arquivo = open("task_02.csv", 'w')
+    arquivo.writelines(task_02.strip())
+    arquivo.close()
+
+    arquivo = open("task_01.csv", 'w')
+    arquivo.writelines(task_1.strip())
+    arquivo.close()
+
+    arquivo = open("task_04.csv", 'w')
+    arquivo.writelines(task_4.strip())
+    arquivo.close()
+
+separar_dados()
